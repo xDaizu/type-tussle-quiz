891a399b96734204508f0ca6b262ad84d0099604
Danny Even <danielparejom@gmail.com>
2025-07-24 10:14:19 +0200

    gh-pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: '/type-tussle-quiz/',
   server: {
     host: "::",
     port: 8080,
