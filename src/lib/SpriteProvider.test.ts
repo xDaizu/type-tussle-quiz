f93a48f95d57405c0f50e240807d9b1c558bc30b
Danny Even <danielparejom@gmail.com>
2025-07-25 11:15:15 +0200

    fix SpriteProvider test

--- src/lib/SpriteProvider.test.ts
+++ src/lib/SpriteProvider.test.ts
@@ -13,13 +13,13 @@ describe('SpriteProvider', () => {
   });
 
   it('getTypeSpriteWithWord returns correct path', () => {
-    expect(SpriteProvider.getTypeSpriteWithWord(PokemonType.Fire)).toBe('sprites/types/wordy/FireIC_SV.png');
-    expect(SpriteProvider.getTypeSpriteWithWord(PokemonType.Water)).toBe('sprites/types/wordy/WaterIC_SV.png');
+    expect(SpriteProvider.getTypeSpriteWithWord(PokemonType.Fire)).toBe('/sprites/types/wordy/FireIC_SV.png');
+    expect(SpriteProvider.getTypeSpriteWithWord(PokemonType.Water)).toBe('/sprites/types/wordy/WaterIC_SV.png');
   });
 
   it('getTypeSpriteSymbol returns correct path', () => {
-    expect(SpriteProvider.getTypeSpriteSymbol(PokemonType.Fire)).toBe('sprites/types/symbols/Fire_icon_Sleep.png');
-    expect(SpriteProvider.getTypeSpriteSymbol(PokemonType.Water)).toBe('sprites/types/symbols/Water_icon_Sleep.png');
+    expect(SpriteProvider.getTypeSpriteSymbol(PokemonType.Fire)).toBe('/sprites/types/symbols/Fire_icon_Sleep.png');
+    expect(SpriteProvider.getTypeSpriteSymbol(PokemonType.Water)).toBe('/sprites/types/symbols/Water_icon_Sleep.png');
   });
 
   it('isLeftFacing returns true for left-facing Pokémon', () => {
